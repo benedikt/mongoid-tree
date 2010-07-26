97a376460d448f67eaa1675d6470f75413f808ca
benedikt <benedikt@synatic.net>
2010-07-26 17:12:49 +0200

    Added some macros to help setting up trees in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,16 +6,16 @@ require 'mongoid/tree'
 
 require 'rspec'
 
-Dir['#{File.dirname(__FILE__)}/support/**/*.rb'].each { |f| require f }
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db('mongoid_tree_test')
+  config.allow_dynamic_fields = false
+end
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.after :each do
     Mongoid.master.collections.reject { |c| c.name =~ /^system\./ }.each(&:drop)
   end
-end
-
-Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db('mongoid_tree_test')
-  config.allow_dynamic_fields = false
 end
---
+++ spec/support/macros/tree_macros.rb
@@ -0,0 +1,46 @@
+require 'yaml'
+
+module Mongoid::Tree::TreeMacros
+  
+  def setup_tree(tree)
+    create_tree(YAML.load(tree))
+  end
+  
+  def node(name) 
+    @nodes[name].reload
+  end
+  
+  def print_tree(node, print_ids = false, depth = 0)
+    print '  ' * depth
+    print '- ' unless depth == 0
+    print node.name
+    print " (#{node.id})" if print_ids
+    print ':' if node.children.any?
+    print "\n"
+    node.children.each { |c| print_tree(c, print_ids, depth + 1) }
+  end
+  
+  private 
+  
+  def create_tree(object)
+    case object
+      when String: return create_node(object)
+      when Array: object.each { |tree| create_tree(tree) }
+      when Hash: 
+        name, children = object.first
+        node = create_node(name)
+        children.each { |c| node.children << create_tree(c) }
+        return node
+    end
+  end
+  
+  def create_node(name)
+    @nodes ||= HashWithIndifferentAccess.new
+    @nodes[name] = Node.create(:name => name)
+  end
+  
+end
+
+RSpec.configure do |config|
+  config.include Mongoid::Tree::TreeMacros
+end
