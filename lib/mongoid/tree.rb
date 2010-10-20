365efa0fa27a709f6a31f762174b1f48c05e682a
benedikt <benedikt@synatic.net>
2010-10-20 22:22:10 +0200

    Don't require saved parents when adding children to them. Closes #4

--- lib/mongoid/tree.rb
+++ lib/mongoid/tree.rb
@@ -84,7 +84,12 @@ module Mongoid # :nodoc:
     include Traversal
 
     included do
-      references_many :children, :class_name => self.name, :foreign_key => :parent_id, :inverse_of => :parent
+      references_many :children, :class_name => self.name, :foreign_key => :parent_id, :inverse_of => :parent do
+        def <<(*objects)
+          super; objects.each { |c| c.parent = @parent }
+        end
+      end
+
       referenced_in :parent, :class_name => self.name, :inverse_of => :children, :index => true
 
       field :parent_ids, :type => Array, :default => []
@@ -289,7 +294,7 @@ module Mongoid # :nodoc:
 
     def rearrange
       if self.parent_id
-        self.parent_ids = base_class.find(self.parent_id).parent_ids + [self.parent_id]
+        self.parent_ids = parent.parent_ids + [self.parent_id]
       else
         self.parent_ids = []
       end
