6e2e845c4cf0e99455d5a96d0d941cfba5ab352c
benedikt <benedikt@synatic.net>
2010-10-20 18:43:31 +0200

    Added #sibling_of?(other) utility method

--- spec/mongoid/tree_spec.rb
+++ spec/mongoid/tree_spec.rb
@@ -278,6 +278,16 @@ describe Mongoid::Tree do
       it "#siblings_and_self should return the documents siblings and itself" do
         node(:child).siblings_and_self.to_a.should == [node(:child), node(:other_child)]
       end
+
+      describe '#sibling_of?' do
+        it "should return true for siblings" do
+          node(:child).should be_sibling_of(node(:other_child))
+        end
+
+        it "should return false for non-siblings" do
+          node(:root).should_not be_sibling_of(node(:other_child))
+        end
+      end
     end
 
     describe '#leaves' do
